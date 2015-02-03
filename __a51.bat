ceaa32d5c9f568c38612b6032bcbeb253aeb082a
Leighton <LcarmichaelPowell@Gmail.com>
2015-02-03 10:21:53 -0800

    fixes

--- __a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\Leighton Carmichael\Documents\GitStuff\Oven_Reflow\"
-"C:\CrossIDE\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Leighton Carmichael\Documents\GitStuff\Oven_Reflow\Oven_Reflow.asm"
-echo Crosside_Action Set_Hex_File C:\Users\Leighton Carmichael\Documents\GitStuff\Oven_Reflow\Oven_Reflow.HEX
